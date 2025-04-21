4e5f74808e19cf0d905f303daa751694503f3516
Jessica <jessica.huang@ch-si.com.tw>
2025-04-21 16:19:10 +0800

    傳上去github-page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // https://你的帳號.github.io/你的-repo-name/
+  // base 前後都有 /，而且大小寫要和 GitHub repo 名稱完全一致
+  base: '/vue3-vite-profolio/', 
   plugins: [vue()],
   resolve: {
     alias: {
